fc4be44991ab3f6c2ddddd26ac31677e1b90caf6
aakash2809 <aakashrajak289@gmail.com>
2020-10-30 16:05:21 +0530

    Add : store results in dictionary

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -1,10 +1,16 @@
-#!/bin/bash 
+#!/bin/bash -x
+
+#TO DECLEAR THE DICTIONARY
+declare -A arithmeticOperation
+
 echo "                   Arithmetic Compution And Sorting"
 echo "--------------------------------------------------------------------"
+
 #TO TAKING INPUT FROM USER
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+
 #COMPUTE ARITHMETIC OPERATION
 result1=`echo $firstInput $secondInput $thirdInput |awk '{printf($1 + $2 * $3)}'`
 echo "Result1: firstInput + secondInput * thirdInput = " $result1
@@ -15,4 +21,8 @@ echo "Result3: firstInput / secondInput + thirdInput = " $result3
 result4=`echo $firstInput $secondInput $thirdInput |awk '{printf($1 % $2 + $3)}'`
 echo "Result4: firstInput % secondInput + thirdInput = " $result4
 
-
+#TO STORE THE DICTIONARY
+arithmeticOperation[((result1))]=$result1
+arithmeticOperation[((result2))]=$result2
+arithmeticOperation[((result3))]=$result3
+arithmeticOperation[((result4))]=$result4
